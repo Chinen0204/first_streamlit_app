517e8e6f3bd7f9c7933cfa5805e2451e51c3659d
Chinen0204 <145743368+Chinen0204@users.noreply.github.com>
2023-09-29 10:30:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,16 +23,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruits_selected_next = streamlit.multiselect("What fruit would you like information about?")
-  if not fruit_choice
-  streamlit.error("Please select a fruit to get information.")
-else:
-  streamlit.write('The user enterd ', fruits_selected_next)
-  fruits_to_show_next = my_fruit_list.loc[fruits_selected_next]
-  streamlit.dataframe(fruits_to_show_next)
-except URLEroor as e:
-streamlit.error()
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 streamlit.stop()
 
